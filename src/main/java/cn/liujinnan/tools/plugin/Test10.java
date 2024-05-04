28dbe273c33adb45499943c267b678a875d38097
liujinnan <740989435@qq.com>
2024-05-04 11:40:01 +0800

    test: 增加测试插件

---
+++ src/main/java/cn/liujinnan/tools/plugin/Test10.java
@@ -0,0 +1,27 @@
+/*
+ * copyright(c) 2024 by liujinnan.cn All rights Reserved.
+ */
+
+package cn.liujinnan.tools.plugin;
+
+import cn.liujinnan.tools.ext.plugin.Plugin;
+import cn.liujinnan.tools.ext.plugin.annotation.PluginComponent;
+
+import javax.swing.*;
+
+/**
+ * @description:
+ * @author: liujinnan
+ * @create: 2024-04-27 14:43
+ **/
+@PluginComponent(name = "test10", icon = "logo.png")
+public class Test10 implements Plugin {
+    @Override
+    public JComponent getJComponent() {
+
+        JPanel jPanel = new JPanel();
+        jPanel.add(new JButton("按钮1"));
+
+        return jPanel;
+    }
+}
