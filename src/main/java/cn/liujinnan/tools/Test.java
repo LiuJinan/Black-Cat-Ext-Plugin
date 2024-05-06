db124381872ca6521722576f3452173526ec3a53
刘锦南 <liujinnan@shuzimofang.net>
2024-05-06 15:26:52 +0800

    test: 增加测试类

---
+++ src/main/java/cn/liujinnan/tools/Test.java
@@ -0,0 +1,16 @@
+package cn.liujinnan.tools;
+
+import cn.liujinnan.tools.ext.plugin.test.TestPlugin;
+import cn.liujinnan.tools.plugin.ip.IpPlugin;
+
+/**
+ * @author ljn
+ * @version 1.0
+ * @date 2024-05-06 14:42
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        TestPlugin.runTest(new IpPlugin());
+    }
+}
